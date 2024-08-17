223166d3d6796ad1940bbf0da5e4521c700095ae
Naveen Sharma <nssharma309@gmail.com>
2024-08-17 16:42:22 +0530

    Changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('Click on get quote')
 
+
+  const getQuote = async()=>{
+    const resp = await fetch('https://api.adviceslip.com/advice');
+    const data = await resp.json(); 
+    console.log(data)
+    setQuote(data.slip.advice)
+    return data;
+  }
+    useEffect(()=>{
+      getQuote()
+    },[])
+  
   return (
     <>
-     <h1>Hello I am naveen</h1>
+    <div className="container">
+      <div className="area">
+        <h1 id='quote'>{quote}</h1>
+      </div>
+        <button  id='btngetQuote' onClick={getQuote}>Get Quote</button>
+    </div>
     </>
   )
+  
+ 
+  
+
 }
 
 export default App
